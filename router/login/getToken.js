ec0804e09c9694bb1537146b48cef66f8c6d4423
zpkweb <zpkafk@icloud.com>
2017-12-15 18:06:09 +0800

    koa

--- router/login/getToken.js
+++ router/login/getToken.js
@@ -2,7 +2,9 @@ const Request = require('../../request')
 const COMMON = require('../../global/constant');
 
 module.exports = async (ctx, next) => {
+	console.log(COMMON.product[ctx.query.type])
  	await Request.ajax('gettoken',COMMON.product[ctx.query.type]).then((res) => {
+ 		console.log(res)
   	ctx.state.token = res.data.token;
   	return next();
   })
