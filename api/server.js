525001e5c2c725620c6e6b4a59aee6e43fc6b636
zpkweb <zpkafk@icloud.com>
2018-02-09 18:01:23 +0800

    modify

--- api/server.js
+++ api/server.js
@@ -2,11 +2,13 @@ const Koa = require('koa');
 const cors = require('koa2-cors');
 const koaBody = require('koa-body');
 const koaBodyparser = require('koa-bodyparser');
-const router = require('./router/config');
+
 const app = new Koa();
+const router = require('./router/init');
 
 app.use(cors());
 app.use(koaBodyparser());
 app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(3080);
+console.log("http://localhost:3080/api/userAction/scene/mobileIndex/login")
